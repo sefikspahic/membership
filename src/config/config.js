d4864d93b33be19feffb33a6667bd64da60e3ffb
sefikspahice <spahicsefik@gmail.com>
2021-10-18 17:35:57 +0200

    done

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 const config = {
     port: process.env.PORT || 5000,
     secret: process.env.JWT_SECRET || 'Your secret key',
-    mongo: process.env.MONGO || "mongodb://loclahost:27017/mernproject"
+    mongo: process.env.MONGO || "mongodb://localhost:27017/mernproject"
 }
 export default config;
