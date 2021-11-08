274a34dd7a85ff7f46fd674d84be42e134f10479
sefikspahice <spahicsefik@gmail.com>
2021-11-08 23:35:50 +0100

    merne project

---
+++ mern-frontend/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const htmlPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "src"),
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "/",
+    sourceMapFilename: "main.js.map",
+  },
+  devtool: "source-map",
+  devServer: {
+    static: { directory: path.join(__dirname, "dist") },
+    compres: true,
+    port: 3000,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
+        use: "file-loader",
+      },
+    ],
+  },
+  plugins: [
+    new htmlPlugin({
+      title: "Membership Application",
+      favicon: "./src/assets/paragin.png",
+      template: "./src/assets/index.html",
+    }),
+  ],
+};
