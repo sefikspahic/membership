8bed9747b9c5a2c980f46805fdb4d8fee0f1c49d
sefikspahice <spahicsefik@gmail.com>
2021-11-11 00:26:28 +0100

    done

--- mern-backend/src/config/config.js
+++ mern-backend/src/config/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const config = {
-    port: process.env.PORT || 5000,
+    port: process.env.PORT || 4400,
     secret: process.env.JWT_SECRET || 'secret-key',
     mongo: process.env.MONGO || "mongodb://localhost:27017/mernproject"
 }
--- mern-frontend/src/auth/PrivateRoute.js
+++ mern-frontend/src/auth/PrivateRoute.js
@@ -16,5 +16,4 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
     }
   />
 );
-
 export default PrivateRoute;
